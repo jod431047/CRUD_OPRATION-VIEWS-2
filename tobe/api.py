052efe3fea4af5f948aa08f4bfdf26f40ed872b4
jod431047 <jod431047@gmail.com>
2023-12-11 14:48:01 +0100

    add filter

--- tobe/api.py
+++ tobe/api.py
@@ -1,14 +1,15 @@
 from.models import Tobe
-from.filters import TobeFilter
 from.serializers import TobeSerializers
-from rest_framework import filters
 from rest_framework import viewsets
+
+from.filters import TobeFilter
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TobeViewset(viewsets.ModelViewSet):
     queryset = Tobe.objects.all()
     serializer_class = TobeSerializers
-    filter_backends = [DjangoFilterBackend,filters.OrderingFilter]
-    filterset_class = TobeFilter     
-    ordering_fields = ['id']  
+    filter_backends = [DjangoFilterBackend]         # django filter search
+    filterset_fields = ['name','description','status']            # django filter search
+ 
